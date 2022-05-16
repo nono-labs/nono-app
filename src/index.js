8e9553ead0cddc73d79cf6fcd6687a22ba8d379b
clayerton <clayerton@163.com>
2022-05-16 14:51:57 +0800

    fix: react-dom/client and mui4.0 imcompatible

--- src/index.js
+++ src/index.js
@@ -3,25 +3,38 @@ import "@fontsource/roboto";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import store from "./store";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </Provider>
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <Provider store={store}>
+//     <BrowserRouter>
+//       <ThemeProvider theme={theme}>
+//         <CssBaseline />
+//         <App />
+//       </ThemeProvider>
+//     </BrowserRouter>
+//   </Provider>
+// );
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
